08354d145c009bf68da666e2908f80adc7cd3129
khudadad <khudadad.khawari1378@gmail.com>
2021-09-02 00:29:33 +0530

    database migrated

--- djangoproject/projects/views.py
+++ djangoproject/projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def projects(request):
+    # return HttpResponse('This is the Multiple Projects page')
+    return render(request, 'projects.html')
+
+def project(request):
+    # return HttpResponse('This is a single project page')
+    return render(request, 'single-project.html')
 
 # Create your views here.
